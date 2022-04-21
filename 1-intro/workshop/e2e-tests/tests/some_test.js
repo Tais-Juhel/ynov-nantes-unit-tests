ef790772c2031de14f80a36cc83aa26b4e0dbd65
medi.demirdelen <medi.demirdelen@gmail.com>
2022-04-21 14:48:48 +0200

    test e2e

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -2,4 +2,12 @@ Feature('Ynov Nantes');
 
 Scenario('Test Ynov Nantes Land Page', ({ I }) => {
     I.amOnPage('https://www.ynov-nantes.com/');
+
+    I.click({css: '.cta'});
+    I.waitUrlEquals('https://www.ynov-nantes.com/recherche');
+
+    I.fillField({css: '.searchfield'}, 'info');
+    I.wait(5);
+
+    I.see('Bachelor Informatique');
 });
