97dc2b867b53ad758c3a333d19cfeba0d6325081
Tais-Juhel <juhel.tais@ynov.vom>
2022-04-21 15:31:09 +0200

    flatten tests

--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -8,6 +8,26 @@ class FlattenTest extends TestCase
 {
     public function test_something()
     {
-        $this->assertEqual(1337, 1337);
+        $this->assertEquals(1337, 1337);
+    }
+
+    public function test_with_null ()
+    {
+        $this->assertEquals([], []);
+    }
+
+    public function test_with_string ()
+    {
+        $this->assertEquals("a", "a");
+    }
+
+    public function test_with_string_in_array ()
+    {
+        $this->assertEquals(["1", 2], ["1",2]);
+    }
+
+    public function test_with_array ()
+    {
+        $this->assertEquals([[1,2], 3], [[1,2],3]);
     }
 }
