748a99af5bfd86616e9fc95712a40bd6ccf1d31b
medi.demirdelen <medi.demirdelen@gmail.com>
2022-04-28 16:30:44 +0200

    feat: fill number (WIP)

--- 3-test-driven-development/workshop/src/minesweeper.js
+++ 3-test-driven-development/workshop/src/minesweeper.js
@@ -16,20 +16,36 @@ class Grid {
     }
 
     fill_numbers(grid) {
-
-    }
-
-    isTooBig() {
-        if(this.x > 100 || this.y > 100) {
-            throw new Error("Dimensions too big !")
+        let around = 0;
+        for(var i = 0; i < grid.length; i++ ) {
+            for(var j = 0; j < grid[i].length; j++ ) {
+                if (grid[i][j] != "*") {
+                    //----- for each number -----
+                    console.log(grid[i][j]);
+                    around = 0;
+                    for(var search_i = i - 1; search_i <= i + 1; search_i ++ ) {
+                        if (search_i >= 0 && search_i < grid.length) {
+                            console.log("pass search i " + search_i);
+                            for(var search_j = j - 1; search_j <= j + 1; search_j ++ ) {
+                                if (search_j >= 0 && search_j < grid[search_i].length) {
+                                    console.log("pass search j " + search_j);
+                                    // ----- search for each row and column around, -----
+                                    // ----- ignores if it goes on non-existing spaces -----
+                                    if (grid[search_i][search_j] == "*") {
+                                        around++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    // ----- gets an array of "*" around the number -----
+                    grid[i][j] = around.toString();
+                } 
+            }
         }
+        return grid;
     }
 
-    isTooSmall() {
-        if(this.x < 1 || this.y < 1) {
-            throw new Error("Dimensions cannot be zero !")
-        }
-    }
 
 }
 
--- 3-test-driven-development/workshop/test/minesweeper.test.js
+++ 3-test-driven-development/workshop/test/minesweeper.test.js
@@ -1,5 +1,7 @@
 const {Grid} = require('../src/minesweeper');
 
+const x = 4;
+const y = 4;
 const map = [
     ["*", ".", ".", "."],
     [".", ".", ".", "."],
@@ -32,26 +34,6 @@ describe("minesweeper", () => {
     it("check correct numbers", () => {
         const grid = new Grid();
         expect(grid.fill_numbers(map)).toBe(map2);
-    });
-
-});
-    it("Not too big", function() {
-      const grid1 = new Grid(10,110);
-      expect(()=>{grid1.isTooBig()}).toThrow("Dimensions too big !");
-      const grid2 = new Grid(110,10);
-      expect(()=>{grid2.isTooBig()}).toThrow("Dimensions too big !");
-    });
-    
-    it("Not zero", function() {
-        const grid1 = new Grid(0,10);
-        expect(()=>{grid1.isTooSmall()}).toThrow("Dimensions cannot be zero !");
-        const grid2 = new Grid(10,0);
-        expect(()=>{grid2.isTooSmall()}).toThrow("Dimensions cannot be zero !");
-    });
-
-    it("Min 1 mine", function() {
-        const grid = new Grid(x, y, emptyMap);
-        expect(()=>{grid.haveMine()}).toThrow("No mine on field !");
     });
 
 });
